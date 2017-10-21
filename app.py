6a34254ad6059d6fcb3657c654640a363987b6e0
Sriram Sami <frizensami@gmail.com>
2017-10-21 23:41:03 +0800

    Refine home, do about and contact pages

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
